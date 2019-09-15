b77e800ee778b578c8c1ef68e96ddf7fc490b317
The Coding Aviator <34807532+thecodingaviator@users.noreply.github.com>
2019-09-15 12:59:03 +0530

    Till card hover

---
+++ main.js
@@ -0,0 +1,33 @@
+let Imagedata = []
+
+const request = async () => {
+
+  const response = await fetch('https://api.github.com/repos/dhanishgajjar/vscode-icons/contents/svg')
+  rawData = await response.json()
+
+  rawData.forEach(element => {
+    var userAction = async () => {
+      if(element.name != 'ratatouille-remi.svg' && element.anem != 'matrix.svg') {
+        var response = await fetch(`https://api.github.com/repos/dhanishgajjar/vscode-icons/contents/svg/${element.name}`);
+        var myJson = await response.json(); //extract JSON from the http response
+        Imagedata.push({json: myJson, name: element.name.toUpperCase().replace(/-/g, ' ').replace('.SVG','').replace('-', ' ').replace('_', ' ')})
+      }
+    }
+
+    userAction()
+  })
+
+  setTimeout(()=>upload(Imagedata), 5000)
+  
+}
+
+function upload(Imagedata) {
+  for(var i = 0; i < Imagedata.length; i++) {
+    if(Imagedata[i].json != undefined && Imagedata[i].name != 'MATRIX') {
+      var html = `<div class="card"><img src="data:image/svg+xml;base64,${Imagedata[i].json.content}"/> <h3>${Imagedata[i].name}</h3></div>`
+      document.getElementById('displayer').innerHTML += html;
+    }
+  };
+}
+
+request()
